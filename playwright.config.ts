1cb49fd7ff534ff0088a708b8b05a3077f678375
darenceT12 <darence.thong@dexcarehealth.com>
2026-03-08 18:10:46 -0700

    early guidance date picker

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,10 +2,11 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
+  timeout: 10000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   reporter: 'list',
   snapshotPathTemplate: '{testDir}/{testFileName}-snapshots/{arg}-{projectName}{ext}',
   use: {
